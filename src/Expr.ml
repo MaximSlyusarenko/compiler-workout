92d799b5823ac1442f30e20edd296532b5d5981f
MaximSlyusarenko <slyuslyu.m@mail.ru>
2018-02-17 15:56:54 +0300

    expression evaluation

--- src/Expr.ml
+++ src/Expr.ml
@@ -33,15 +33,19 @@ let empty = fun x -> failwith (Printf.sprintf "Undefined variable %s" x)
 let update x v s = fun y -> if x = y then v else s y
 
 (* An example of a non-trivial state: *)                                                   
-let s = update "x" 1 @@ update "y" 2 @@ update "z" 3 @@ update "t" 4 empty
+(*let s = update "x" 1 @@ update "y" 2 @@ update "z" 3 @@ update "t" 4 empty*)
 
 (* Some testing; comment this definition out when submitting the solution. *)
-let _ =
-  List.iter
-    (fun x ->
-       try  Printf.printf "%s=%d\n" x @@ s x
-       with Failure s -> Printf.printf "%s\n" s
-    ) ["x"; "a"; "y"; "z"; "t"; "b"]
+(*let _ =*)
+  (*List.iter*)
+    (*(fun x ->*)
+       (*try  Printf.printf "%s=%d\n" x @@ s x*)
+       (*with Failure s -> Printf.printf "%s\n" s*)
+    (*)) ["x"; "a"; "y"; "z"; "t"; "b"]*)
+
+let intToBool x = x != 0
+
+let boolToInt x = if x then 1 else 0
 
 (* Expression evaluator
 
@@ -50,5 +54,24 @@ let _ =
    Takes a state and an expression, and returns the value of the expression in 
    the given state.
 *)
-let eval = failwith "Not implemented yet"
-                    
+let rec eval state expression = match expression with
+  | Const x -> x
+  | Var x -> state x
+  | Binop ("!!", x, y) -> evalBooleanFunctionWithBooleanArgs (||) state x y
+  | Binop ("&&", x, y) -> evalBooleanFunctionWithBooleanArgs (&&) state x y
+  | Binop ("==", x, y) -> evalBooleanFunctionWithIntArgs (==) state x y
+  | Binop ("!=", x, y) -> evalBooleanFunctionWithIntArgs (!=) state x y
+  | Binop ("<=", x, y) -> evalBooleanFunctionWithIntArgs (<=) state x y
+  | Binop ("<", x, y) -> evalBooleanFunctionWithIntArgs (<) state x y
+  | Binop (">=", x, y) -> evalBooleanFunctionWithIntArgs (>=) state x y
+  | Binop (">", x, y) -> evalBooleanFunctionWithIntArgs (>) state x y
+  | Binop ("+", x, y) -> evalIntFunctionWithIntArgs (+) state x y
+  | Binop ("-", x, y) -> evalIntFunctionWithIntArgs (-) state x y
+  | Binop ("*", x, y) -> evalIntFunctionWithIntArgs ( * ) state x y
+  | Binop ("/", x, y) -> evalIntFunctionWithIntArgs (/) state x y
+  | Binop ("%", x, y) -> evalIntFunctionWithIntArgs (mod) state x y
+  | _ -> failwith "Wrong expression"
+
+  and evalBooleanFunctionWithBooleanArgs func state x y = boolToInt (func (intToBool (eval state x)) (intToBool (eval state y)))
+  and evalBooleanFunctionWithIntArgs func state x y = boolToInt (func (eval state x) (eval state y))
+  and evalIntFunctionWithIntArgs func state x y = func (eval state x) (eval state y)
