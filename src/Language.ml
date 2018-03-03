0043a8a34c00006e08672b4b9419181707ea4a06
MaximSlyusarenko <slyuslyu.m@mail.ru>
2018-03-03 15:15:46 +0300

    Homework 3 (parser) done

--- src/Language.ml
+++ src/Language.ml
@@ -2,9 +2,11 @@
    The library provides "@type ..." syntax extension and plugins like show, etc.
 *)
 open GT
+open List
 
 (* Opening a library for combinator-based syntax analysis *)
 open Ostap.Combinators
+open Ostap
        
 (* Simple expressions: syntax and semantics *)
 module Expr =
@@ -37,14 +39,39 @@ module Expr =
     *)
     let update x v s = fun y -> if x = y then v else s y
 
-    (* Expression evaluator
+    let intToBool x = x != 0
 
-          val eval : state -> t -> int
+    let boolToInt x = if x then 1 else 0
+
+    (* Expression evaluator
+       val eval : state -> expr -> int
  
        Takes a state and an expression, and returns the value of the expression in 
        the given state.
     *)
-    let eval _ = failwith "Not implemented yet"
+    let rec eval state expression = match expression with
+      | Const x -> x
+      | Var x -> state x
+      | Binop ("!!", x, y) -> evalBooleanFunctionWithBooleanArgs (||) state x y
+      | Binop ("&&", x, y) -> evalBooleanFunctionWithBooleanArgs (&&) state x y
+      | Binop ("==", x, y) -> evalBooleanFunctionWithIntArgs (==) state x y
+      | Binop ("!=", x, y) -> evalBooleanFunctionWithIntArgs (!=) state x y
+      | Binop ("<=", x, y) -> evalBooleanFunctionWithIntArgs (<=) state x y
+      | Binop ("<", x, y) -> evalBooleanFunctionWithIntArgs (<) state x y
+      | Binop (">=", x, y) -> evalBooleanFunctionWithIntArgs (>=) state x y
+      | Binop (">", x, y) -> evalBooleanFunctionWithIntArgs (>) state x y
+      | Binop ("+", x, y) -> evalIntFunctionWithIntArgs (+) state x y
+      | Binop ("-", x, y) -> evalIntFunctionWithIntArgs (-) state x y
+      | Binop ("*", x, y) -> evalIntFunctionWithIntArgs ( * ) state x y
+      | Binop ("/", x, y) -> evalIntFunctionWithIntArgs (/) state x y
+      | Binop ("%", x, y) -> evalIntFunctionWithIntArgs (mod) state x y
+      | _ -> failwith "Wrong expression"
+
+    and evalBooleanFunctionWithBooleanArgs func state x y = boolToInt (func (intToBool (eval state x)) (intToBool (eval state y)))
+    and evalBooleanFunctionWithIntArgs func state x y = boolToInt (func (eval state x) (eval state y))
+    and evalIntFunctionWithIntArgs func state x y = func (eval state x) (eval state y)
+
+    let constructOpsList lst = List.map (fun op -> (ostap ($(op)), fun x y -> Binop (op, x, y))) lst
 
     (* Expression parser. You can use the following terminals:
 
@@ -53,7 +80,19 @@ module Expr =
    
     *)
     ostap (
-      parse: empty {failwith "Not implemented yet"}
+      primary: s:IDENT {Var s} | x:DECIMAL {Const x} | -"(" parse -")";
+
+      parse: !(Util.expr
+        (fun x -> x)
+        [|
+          `Lefta, constructOpsList ["!!"];
+          `Lefta, constructOpsList ["&&"];
+          `Nona,  constructOpsList ["<="; ">="; "<"; ">"; "=="; "!="];
+          `Lefta, constructOpsList ["+"; "-"];
+          `Lefta, constructOpsList["*"; "/"; "%"]
+        |]
+        primary
+      )
     )
 
   end
@@ -78,11 +117,23 @@ module Stmt =
 
        Takes a configuration and a statement, and returns another configuration
     *)
-    let eval _ = failwith "Not implemented yet"
+    let rec eval (state, input, output) expression = match expression with
+      | Read s -> (Expr.update s (hd input) state, tl input, output)
+      | Write expr -> (state, input, (Expr.eval state expr) :: output)
+      | Assign (s, expr) -> (Expr.update s (Expr.eval state expr) state, input, output)
+      | Seq (st1, st2) -> eval (eval (state, input, output) st1) st2
 
     (* Statement parser *)
     ostap (
-      parse: empty {failwith "Not implemented yet"}
+      primary: -"read" -"(" z:IDENT -")" {Read z} | -"write" -"(" e:!(Expr.parse) -")" {Write e} | left:IDENT -":=" right:!(Expr.parse) {Assign (left, right)};
+
+      parse: !(Ostap.Util.expr
+        (fun x -> x)
+        [|
+          `Righta, [ostap (";"), fun x y -> Seq(x, y)]
+        |]
+        primary
+      )
     )
       
   end
--- src/SM.ml
+++ src/SM.ml
@@ -1,9 +1,11 @@
-open GT       
+open GT
+open List
 open Language
        
 (* The type for the stack machine instructions *)
 @type insn =
 (* binary operator                 *) | BINOP of string
+(* put a constant on the stack     *) | CONST of int                 
 (* read to stack                   *) | READ
 (* write from stack                *) | WRITE
 (* load a variable to the stack    *) | LD    of string
@@ -15,31 +17,52 @@ type prg = insn list
 (* The type for the stack machine configuration: a stack and a configuration from statement
    interpreter
  *)
-type config = int list * Stmt.config
+type config = int list * Language.Stmt.config
+
+(*
+	Evaluates single operation of stack machine
+	val evalSingleOperation : config -> insn -> config
+*)
+let evalSingleOperation (stack, (state, input, output)) operation = match operation with
+	| BINOP op -> 
+		let hd1 :: hd2 :: tl = stack in
+		let const1 = Expr.Const hd1 in
+		let const2 = Expr.Const hd2 in
+		let res = Expr.eval state (Expr.Binop (op, const1, const2)) in
+		(res :: tl, (state, input, output))
+	| CONST x -> (x :: stack, (state, input, output))
+	| READ -> ((hd input) :: stack, (state, tl input, output))
+	| WRITE -> (tl input, (state, input, (hd stack) :: output))
+	| LD s -> ((state s) :: stack, (state, input, output))
+	| ST s -> (tl stack, (Expr.update s (hd stack) state, input, output))
 
 (* Stack machine interpreter
-
      val eval : config -> prg -> config
-
    Takes a configuration and a program, and returns a configuration as a result
- *)                         
-let eval _ = failwith "Not yet implemented"
+ *)     
+let rec eval fullConfig program = match program with
+	| [] -> fullConfig
+	| hd :: tl -> eval (evalSingleOperation fullConfig hd) tl
+
+let rec compileExpression expr = match expr with
+	| Expr.Const n -> [CONST n]
+	| Expr.Var s -> [LD s]
+	| Expr.Binop (operation, x, y) -> (compileExpression x) @ (compileExpression y) @ [BINOP operation]
 
 (* Top-level evaluation
-
      val run : prg -> int list -> int list
-
    Takes an input stream, a program, and returns an output stream this program calculates
 *)
-let run p i = let (_, (_, _, o)) = eval ([], (Expr.empty, i, [])) p in o
+let run p i = let (_, (_, _, o)) = eval ([], (Expr.empty, i, [])) p in o	
 
 (* Stack machine compiler
-
-     val compile : Language.Stmt.t -> prg
-
+     val compile : Syntax.Stmt.t -> prg
    Takes a program in the source language and returns an equivalent program for the
    stack machine
  *)
-let compile _ = failwith "Not yet implemented"
 
-                         
+let rec compile statement = match statement with
+	| Stmt.Read s -> [READ ; ST s]
+	| Stmt.Write expr -> (compileExpression expr) @ [WRITE]
+	| Stmt.Assign (s, expr) -> (compileExpression expr) @ [ST s]
+	| Stmt.Seq (st1, st2) -> (compile st1) @ (compile st2)
